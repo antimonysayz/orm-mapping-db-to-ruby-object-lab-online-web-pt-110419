6d9256ffe69696e5030739e436557bb5cfe26ee8
Anthony Mai <anthony.mai530@gmail.com>
2020-01-30 02:22:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,6 +53,17 @@ class Student
     new_from_db(first)
   end
 
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, num).map{
+      |row|
+      new_from_db(row)
+    }
+  end
+
   def self.all
     sql = <<-SQL
     SELECT * FROM students
